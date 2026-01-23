a20dadec9673f214059af2f69504b918a4f38477
InscrutableOwl <mttym42@icloud.com>
2026-01-23 09:47:36 -0500

    removing geany

--- mx/package.sh
+++ mx/package.sh
@@ -2,5 +2,5 @@
 
 ## Mint packages
 
-sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay cowsay-off sl btop cmatrix cmatrix-xfont bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima geany geany-plugins geany-plugin-addons geany-plugin-git-changebar geany-plugin-treebrowser -y
+sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay cowsay-off sl btop cmatrix cmatrix-xfont bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima -y
 
