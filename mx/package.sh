3d1bc6c71483e58e4373d16c9b9f1fcf5600743a
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-01-23 13:48:00 -0500

    Add qtile and lightdm packages to installation script

--- mx/package.sh
+++ mx/package.sh
@@ -2,5 +2,5 @@
 
 ## Mint packages
 
-sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay cowsay-off sl btop cmatrix cmatrix-xfont bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima -y
+sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay cowsay-off sl btop cmatrix cmatrix-xfont bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima qtile lxappearance lightdm-settings slick-greeter -y
 
