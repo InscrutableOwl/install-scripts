43caa2d7e2d3410d4554fc66a315989eebf308c9
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-01-27 19:44:29 -0500

    Remove background image copy commands
    Remove background image copy commands from move.sh

--- move.sh
+++ move.sh
@@ -6,9 +6,3 @@ sudo cp -r ~/dotfiles/themes/* ~/.themes
 cp -r ~/dotfiles/Pictures/* ~/Pictures
 # mkdir -p ~/Games
 # mv ~/scripts/Roms/* ~/Games
-
-sudo cp ~/dotfiles/Pictures/gruvbox_tux.png /usr/share/backgrounds
-sudo cp ~/dotfiles/Pictures/mint.png /usr/share/backgrounds
-sudo cp ~/dotfiles/Pictures/gruvbox-1.png /usr/share/backgrounds
-sudo cp ~/dotfiles/Pictures/awesome.png /usr/share/backgrounds
-sudo cp ~/dotfiles/Pictures/mad.png /usr/share/backgrounds
