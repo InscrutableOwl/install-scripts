a28bf6caaa8259bb7d7aa05ba20c9ac92596b280
InscrutableOwl <148409977+InscrutableOwl@users.noreply.github.com>
2026-01-31 12:48:03 -0500

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@
 
 ## Mint packages
 
-sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay sl btop cmatrix bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima -y
+sudo apt install lolcat innoextract htop stow lame curl fortune-mod fortunes cowsay sl btop cmatrix mplayer bat eza nala vlc qcomicbook audacious cgns-convert wxmaxima -y
 
